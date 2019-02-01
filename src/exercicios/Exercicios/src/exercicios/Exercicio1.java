167a1af66cd72938351f4086132e61e8330f64fe
Juan <Juan@DESKTOP-J5PU6RU>
2019-02-01 17:31:13 +0100

    Windows

---
+++ src/exercicios/Exercicios/src/exercicios/Exercicio1.java
@@ -0,0 +1,34 @@
+
+package exercicios;
+
+public class Exercicio1 extends Thread{
+   
+
+    public Exercicio1(String p) {
+      super(p);
+       
+    }
+
+    public void run(){
+        for(int i=0;i<10;i++){
+          
+        System.out.println(i+getName());
+            long g= (long) (Math.random()*2000);
+           
+              try {
+                Thread.sleep(g);
+                
+            } catch (InterruptedException ex) {
+                System.out.println("ERROR: "+ex);
+            }
+    }  
+        }
+    
+ 
+ 
+    public static void main(String[] args) {        
+    new Exercicio1("h1").start();
+      new Exercicio1("h2").start();   
+    }
+    
+}
