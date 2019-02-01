c0e71a8e3c9fe1ee2fe05866fd0bb7f82bcb475a
oracle <oracle@localhost>
2019-02-01 12:07:37 +0100

    Cambio

---
+++ src/exercicios/Exercicio1.java
@@ -0,0 +1,27 @@
+
+package exercicios;
+
+public class Exercicio1 extends Thread{
+   Thread r;
+
+    public Exercicio1(String p) {
+      super(p);
+       
+    }
+ 
+    public void run(){
+        for(int i=0;i<10;i++){
+          
+        System.out.println(i+getName());
+    }  
+        }
+    
+ 
+ 
+    public static void main(String[] args) {
+        
+    new Exercicio1("hilo1").start();
+      new Exercicio1("hilo2").start();   
+    }
+    
+}
