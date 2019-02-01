a94edba3f40c98102207ce61f6e28e3ba022a15a
oracle <oracle@localhost>
2019-02-01 12:40:57 +0100

    Modificando para parar durante algún tiempo las ejecuciones de los hilos.

--- src/exercicios/Exercicio1.java
+++ src/exercicios/Exercicio1.java
@@ -1,6 +1,9 @@
 
 package exercicios;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Exercicio1 extends Thread{
    
 
@@ -8,11 +11,18 @@ public class Exercicio1 extends Thread{
       super(p);
        
     }
- 
+
     public void run(){
         for(int i=0;i<10;i++){
           
         System.out.println(i+getName());
+            double r=Math.random();
+           long g=(long) r;
+            try {
+                Thread.sleep(g);
+            } catch (InterruptedException ex) {
+                System.out.println("ERROR: "+ex);
+            }
     }  
         }
     
