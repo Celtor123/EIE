7d24e12b59237b52398a280d386a197d264811be
Juan <Juan@DESKTOP-J5PU6RU>
2019-02-03 15:02:33 +0100

    Fin exercicio 5 con prioridades

--- src/exercicios/Exercicios/src/exercicios/Exercicio5.java
+++ src/exercicios/Exercicios/src/exercicios/Exercicio5.java
@@ -3,15 +3,38 @@ package exercicios;
 
 
 public class Exercicio5 extends Thread {
-    
-    public void run(){
         
+    private final int numero;
+   
+     public Exercicio5(int numero) {
+         this.numero=numero;
+         setName(""+numero);
+    }
+    
+    
+ @Override
+    public void run(){
+        try {
+            Thread.sleep(500);
+            System.out.println("Hola, son o fío número"+getName());
+        } catch (InterruptedException ex) {
+            System.out.println("Error: "+ex);
+        }                     
     }
     
     public static void main(String[] args) {
-        
-    }
-}
+        Exercicio5 a= new Exercicio5(1);
+       Exercicio5 b= new Exercicio5(2);
+       a.setPriority(MIN_PRIORITY);
+       b.setPriority(MAX_PRIORITY);
+        b.start();               
+//        try {
+//            b.join();
+//        } catch (InterruptedException ex) {
+//            System.out.println("Error"+ex);
+//        }
+       a.start();     
+    }}
 
 //Escribe unha clase que cree dous fíos e force que a escritura do segundo sexa sempre anterior á escritura por pantalla do primeiro.     
 //Exemplo de ejecución:     
